80e367e52bc63e292aa0bce401ccb555d4f81e91
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-05-22 01:00:58 -0700

    #107 solution

---
+++ BinaryTreeLevelOrderII.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // Binary Tree Level Order Traversal - II
+    //      --> well, actually just adding (0, ..) at :29 works
+    // http://leetcode.com/onlinejudge#question_107
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (root == null) return res;
+        
+        TreeNode mark = new TreeNode(Integer.MIN_VALUE);
+        
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(mark);
+        q.offer(root);
+        
+        ArrayList<Integer> level = null;
+        while(!q.isEmpty()){
+            TreeNode deq = q.poll(); // dequeue
+            
+            if (deq == mark){
+                if (level != null && !level.isEmpty()) res.add(0, level);
+                level = new ArrayList<Integer>(); //start new level
+                if (q.isEmpty()) break; // list finished
+                q.offer(mark);
+            } else {
+                // regular node
+                level.add(deq.val);
+                if (deq.left != null) q.offer(deq.left);
+                if (deq.right != null) q.offer(deq.right);
+            }
+        }
+        return res;
+    }
+}
